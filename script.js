86c4dcb8a6ded6db3e52fc9b762a7cb5c2369312
gercamjr <gercamjr.dev@gmail.com>
2022-04-27 10:47:55 -0500

    creating element objects

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('previous')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
 
