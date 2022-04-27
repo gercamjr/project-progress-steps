3eb2ebb87c9e646e79e8390dec81f64a3645aa04
gercamjr <gercamjr.dev@gmail.com>
2022-04-27 10:58:40 -0500

    created update progress step function

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-const progress = document.getElementById('progress')
-const prev = document.getElementById('previous')
-const next = document.getElementById('next')
-const circles = document.querySelectorAll('.circle')
+const progress = document.getElementById("progress");
+const prev = document.getElementById("previous");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
 
-var currentActive = 1
+var currentActive = 1;
 
-next.addEventListener('click', (event) => {
-    currentActive++
+next.addEventListener("click", (event) => {
+  currentActive++;
 
-    if(currentActive > circles.length) {
-        currentActive = circles.length
-    }
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+
+  update()
+
+});
+
+prev.addEventListener("click", (event) => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update()
+});
+
+function update() {
+    circles.forEach((circle, index) => {
+        if (index < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active')
     
-})
+}
